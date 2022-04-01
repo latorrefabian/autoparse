1981f2b73ccb4e9f6a4f69efe8b38cd477756fa5
Fabian Ricardo Latorre Gomez <flatorre@flatorr-ltmrtjx.internal.salesforce.com>
2022-04-01 10:50:52 +0200

    change distutils to setuptools, added dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(name='autoparse',
-      version='0.1',
+      version='0.1.1',
       description='Automatic Argument Parser from Signature and Docstring',
       author='Fabian Latorre',
       author_email='latorrefabian@gmail.com',
       # url='https://www.python.org/sigs/distutils-sig/',
       packages=['autoparse',],
+      install_requires=[
+          'docstring-parser',],
      )
